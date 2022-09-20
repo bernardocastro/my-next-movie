7150d41a3f2c119e62425392f3fde34b6b9c3593
Bernardo Castro <bernardo.abreucastro@gmail.com>
2022-09-20 20:03:49 -0300

    cleaning

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
-import { useState } from 'react'
-
 function Home() {
     return (
         <>
             <div>Home</div>
-            <Contador />
-        </>
-
-    )
-}
-
-const Contador = () => {
-    const [contador, setContador] = useState(1)
-
-    const adicionarContador = () => {
-        setContador(contador + 1)
-    }
-
-    return (
-        <>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
         </>
     )
 }
