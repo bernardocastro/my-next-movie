eb290072d26a28ea660f1e8eac950037a316cf88
Bernardo Castro <bernardo.abreucastro@gmail.com>
2023-01-12 14:07:59 -0300

    Adding login page

---
+++ src/pages/browse.js
@@ -0,0 +1,46 @@
+import Header from '../components/Header/index';
+import MovieCard from '../components/MovieCard/index';
+import Banner from '../components/Banner/index';
+import { Box } from '@mui/material';
+import styled from 'styled-components';
+
+const GenreTitle = styled.p`
+    margin-left: 3rem; 
+    margin-top: 30px; 
+    color: #FFF; 
+    font-size: 1.8rem; 
+    font-weight: 900;
+`
+
+const Browse = () => {
+    return (
+        <div style={{overflowX: 'hidden'}}>
+            <Header />
+            <Banner />
+            <Box style={{ width: 10000 }}>
+                <GenreTitle >
+                    Week Trending
+                </GenreTitle>
+                <MovieCard endpoint={`/trending/movie/week?api_key=${process.env.TMDB_KEY}`} />
+                <GenreTitle>
+                    Comedy
+                </GenreTitle>
+                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=35`} />
+                <GenreTitle>
+                    Horror
+                </GenreTitle>
+                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=27`} />
+                <GenreTitle>
+                    Thriller
+                </GenreTitle>
+                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=53`} />
+                <GenreTitle>
+                    Crime
+                </GenreTitle>
+                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=80`} />
+            </Box>
+        </div>
+    )
+}
+
+export default Browse
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,72 @@
-import Header from '../components/Header/index';
-import MovieCard from '../components/MovieCard/index';
-import Banner from '../components/Banner/index';
-import { Box } from '@mui/material';
+import Link from 'next/link';
 import styled from 'styled-components';
 
-const GenreTitle = styled.p`
-    margin-left: 3rem; 
-    margin-top: 30px; 
-    color: #FFF; 
-    font-size: 1.8rem; 
-    font-weight: 900;
+const LoginBg = styled.img`
+    width: 100%;
+    height: 100%;
+    filter: brightness(0.5)
+`
+const LoginTitle = styled.p`
+    color: #FFF;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 50px;
+    text-align: center;
+    font-weight: 500
+`
+const LoginSubtitle = styled.p`
+    color: #FFF;
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    font-size: 26px
+`
+const LoginButton = styled.button`
+    width: 200px;
+    height: 60px;
+    background: #E50914;
+    color: #FFF;
+    text-align: center;
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    transform: translate(-50%, -70%);
+    border: 1px solid #E50914;
+    border-radius: 3px;
+    font-size: 20px;
+    opacity: .8;
+    &:hover {
+        cursor: pointer;
+        opacity: 1;
+    };
 `
 
 const Home = () => {
     return (
-        <div style={{overflowX: 'hidden'}}>
-            <Header />
-            <Banner />
-            <Box style={{ width: 10000 }}>
-                <GenreTitle >
-                    Week Trending
-                </GenreTitle>
-                <MovieCard endpoint={`/trending/movie/week?api_key=${process.env.TMDB_KEY}`} />
-                <GenreTitle>
-                    Comedy
-                </GenreTitle>
-                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=35`} />
-                <GenreTitle>
-                    Horror
-                </GenreTitle>
-                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=27`} />
-                <GenreTitle>
-                    Thriller
-                </GenreTitle>
-                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=53`} />
-                <GenreTitle>
-                    Crime
-                </GenreTitle>
-                <MovieCard endpoint={`/discover/movie?api_key=${process.env.TMDB_KEY}&with_genres=80`} />
-            </Box>
+        <div style={{height: '100%', overflow: 'hidden'}}>
+            <LoginBg
+                src={'https://assets.nflxext.com/ffe/siteui/vlv3/935156fb-9579-4fc2-ad94-70680402b8ef/9970821c-56b1-4ead-8063-3533c09e0f17/BR-en-20230109-popsignuptwoweeks-perspective_alpha_website_medium.jpg'}
+                alt="Login Background"
+            />
+            <div>
+                <LoginTitle>
+                    Unlimited Movies, TV shows, and more.
+                </LoginTitle>
+                <LoginSubtitle>
+                    Watch anywhere. Cancel anytime.
+                </LoginSubtitle>
+                <Link href="/browse">
+                    <LoginButton>
+                        Get started
+                    </LoginButton>
+                </Link>
+            </div>
         </div>
     )
+
 }
 
 export default Home
