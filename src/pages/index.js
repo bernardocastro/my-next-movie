f953c05a157d43dad2ecea88aeafb5bc3ef6ff9e
Bernardo Castro <bernardo.abreucastro@gmail.com>
2023-01-18 13:28:39 -0300

    Adjusting home mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import styled from 'styled-components';
 const LoginBg = styled.img`
     width: 100%;
     height: 100%;
-    filter: brightness(0.5)
+    filter: brightness(0.5);
+    @media (max-width: 768px) {
+        width: 320%
+    }
 `
 const LoginTitle = styled.p`
     color: #FFF;
@@ -14,7 +17,11 @@ const LoginTitle = styled.p`
     transform: translate(-50%, -50%);
     font-size: 50px;
     text-align: center;
-    font-weight: 500
+    font-weight: 500;
+    @media (max-width: 768px) {
+       font-size: 35px;
+       width: 290px;
+      }
 `
 const LoginSubtitle = styled.p`
     color: #FFF;
@@ -22,7 +29,14 @@ const LoginSubtitle = styled.p`
     top: 60%;
     left: 50%;
     transform: translate(-50%, -60%);
-    font-size: 26px
+    font-size: 26px;
+    @media (max-width: 768px) {
+        width: 315px;
+        top: 61%;
+        left: 53%;
+        font-size: 20px;
+        margin-top: 15px;
+       }
 `
 const LoginButton = styled.button`
     width: 200px;
@@ -42,11 +56,14 @@ const LoginButton = styled.button`
         cursor: pointer;
         opacity: 1;
     };
+    @media (max-width: 768px) {
+        margin-top: 20px;
+       }
 `
 
 const Home = () => {
     return (
-        <div style={{height: '100%', overflow: 'hidden'}}>
+        <div style={{ height: '100%', overflow: 'hidden' }}>
             <LoginBg
                 src={'https://assets.nflxext.com/ffe/siteui/vlv3/935156fb-9579-4fc2-ad94-70680402b8ef/9970821c-56b1-4ead-8063-3533c09e0f17/BR-en-20230109-popsignuptwoweeks-perspective_alpha_website_medium.jpg'}
                 alt="Login Background"
