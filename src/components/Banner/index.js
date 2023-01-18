d44f8f5cbafcb72b1136e190ef77f4d7b242e518
Bernardo Castro <bernardo.abreucastro@gmail.com>
2023-01-18 13:10:30 -0300

    Adjusting mobile browsing

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,16 +1,60 @@
 import { useEffect, useState } from 'react';
 import api from '../../api/api';
-import { Typography, IconButton } from '@mui/material'
+import { IconButton } from '@mui/material'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import styled from 'styled-components';
 
 const BannerImg = styled.img`
     width: 100%;
-    height: 125%; 
+    height: 100%; 
     position: relative;
     -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))
 `
+const TextWrapper = styled.div`
+    margin-left: 3.5rem; 
+    position: absolute;
+    top: 40%; 
+    width: 400;
+    @media (max-width: 768px) {
+        position: relative;
+        top: -38%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-left: 1.5rem;
+      }
+`
+const Title = styled.p`
+    font-weight: 600;
+    font-size: 40px;
+    color: #fff; 
+    text-shadow: 2px 2px 4px rgb(0 0 0 / 45%);
+    @media (max-width: 768px) {
+        font-size: 35px;
+      }
+`
+const OverviewWrapper = styled.div`
+    width: 500px;
+    height: 90px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media (max-width: 768px) {
+        width: 300px;
+        height: 70px;
+      }
+`
+const Overview = styled.p`
+    font-weight: 500;
+    font-size: 20px; 
+    color: #fff; 
+    text-shadow: 2px 2px 4px rgb(0 0 0 / 45%);
+    @media (max-width: 768px) {
+       font-size: 15px
+      }
+`
+
+
 
 const Banner = () => {
 
@@ -34,26 +78,30 @@ const Banner = () => {
                     src={`${process.env.NEXT_PUBLIC_IMAGE_URL}` + randomMovie.backdrop_path}
                     alt={randomMovie.title}
                 />
-                <div style={{ marginLeft: '3.5rem', position: 'absolute', top: '25%', width: 400 }}>
-                    <Typography style={{ fontWeight: 600, fontSize: 40, color: '#fff', textShadow: '2px 2px 4px rgb(0 0 0 / 45%)' }}>{randomMovie.title}</Typography>
-                    <div style={{ width: 500, height: 90, overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                        <Typography style={{ fontWeight: 500, fontSize: 20, color: '#fff', textShadow: '2px 2px 4px rgb(0 0 0 / 45%)' }}>{randomMovie.overview}</Typography>
-                    </div>
+                <TextWrapper>
+                    <Title>
+                        {randomMovie.title}
+                    </Title>
+                    <OverviewWrapper>
+                        <Overview>
+                            {randomMovie.overview}
+                        </Overview>
+                    </OverviewWrapper>
                     <div>
                         <IconButton style={{ width: 120, height: 42, color: '#000', background: '#fff', borderRadius: '0.3rem', margin: '12px 12px 12px 0' }}>
                             <PlayArrowIcon style={{ marginRight: 10 }} />
-                            <Typography style={{ fontWeight: 700, fontSize: 20 }}>
+                            <p style={{ fontWeight: 700, fontSize: 20 }}>
                                 Play
-                            </Typography>
+                            </p>
                         </IconButton>
                         <IconButton style={{ width: 150, height: 42, color: '#fff', background: 'rgba(109, 109, 109, .7)', borderRadius: '0.3rem' }}>
                             <InfoOutlinedIcon style={{ marginRight: 10 }} />
-                            <Typography style={{ fontWeight: 700, fontSize: 20 }}>
+                            <p style={{ fontWeight: 700, fontSize: 20 }}>
                                 More info
-                            </Typography>
+                            </p>
                         </IconButton>
                     </div>
-                </div>
+                </TextWrapper>
             </div>
         </>
     )
